47abf174ab780947be33d8cd22cfa0065189bcc1
vignesh-kv <kv.vignesh2004@gmail.com>
2025-02-09 19:36:39 +0530

    Add files via upload

---
+++ brea.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class brea{
+public static void main(String[]args){
+String s1="e";
+switch(s1)
+{
+case "a":
+System.out.println(0);
+case "b":
+System.out.println(1);
+case "c":
+System.out.println(2);
+case "d":
+System.out.println(3);
+case "e":
+System.out.println(4);
+}
+}
+}
